9d611d3cd0c691722b86bc6c3bec92a5757b5dc9
Gabriel andreatto alves <gabriel.andreatto.1@gmail.com>
2016-11-23 04:01:01 -0200

    Implementacão da API do facebook completa de acordo com combinado !

---
+++ app/src/main/java/com/example/wilian/jogovelha/ResultActivity.java
@@ -0,0 +1,143 @@
+package com.example.wilian.jogovelha;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView userName;
+    CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
+    ProfileTracker profileTracker;
+    ShareDialog shareDialog;
+    //ShareButton shareButton;
+    String nomeCompelto;
+    Button buttonFbShr;
+
+    String derrotas;
+    String empates;
+    String vitorias;
+    String resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        userName = (TextView) findViewById( R.id.textViewUserName );
+        //shareButton = (ShareButton) findViewById( R.id.fb_share_button );
+        buttonFbShr = ( Button ) findViewById( R.id.fb_share_button );
+
+        shareDialog = new ShareDialog(this);
+
+        Bundle result = getIntent().getExtras();
+        if( result != null ) {
+            derrotas = result.getString( "derrotas" );
+            empates = result.getString( "empates" );
+            vitorias = result.getString( "vitorias" );
+        }
+
+        initFbData();
+        setDataUser();
+        fnctBtnShare();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult( requestCode, resultCode, data );
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    private void fnctBtnShare() {
+        buttonFbShr.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ShareDialog.canShow(ShareLinkContent.class)) {
+                    ShareLinkContent content = new ShareLinkContent.Builder()
+                            .setContentUrl(Uri.parse("https://developers.facebook.com/android"))
+                            .setContentTitle("Jogo da Velha # - Sr Batata ")
+                            .setContentDescription(Profile.getCurrentProfile().getFirstName()
+                                    + " estava jogando o "
+                                    + "\n  Jogo da Velha do Sr. Batata "
+                                    + "\n  Placar: D: " + derrotas + " E: " + empates + " V: " + vitorias )
+                            .build();
+                    //buttonFbShr.setShareContent(content);
+                    shareDialog.show(content);
+                }
+            }
+
+        });
+
+    }
+
+    private void initFbData() {
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+            }
+        };
+        // If the access token is available already assign it.
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+                // App code
+
+            }
+        };
+    }
+    
+
+    private void setDataUser() {
+        //Profile profile = Profile.getCurrentProfile();
+        nomeCompelto = Profile.getCurrentProfile().getName();
+        userName.setText( nomeCompelto );
+        ((ProfilePictureView)findViewById(R.id.profilePicture)).setProfileId(
+                Profile.getCurrentProfile().getId()
+        );
+    }
+
+    public void redirecionar(View view) {
+        goMainScreen();
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent( this, MainActivity.class );
+        intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK );
+        startActivity( intent );
+    }
+
+    // Inserir novos metodos abaixo PLS 4x0
+
+}
